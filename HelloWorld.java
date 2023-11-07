ddbce006efdfa6377f1aaa5380767b42e36d258e
shivamani8314 <110388855+shivamani8314@users.noreply.github.com>
2023-11-07 16:10:36 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.*;
+import java.lang.*;
+public class HelloWorld{
+public static void main(String[] args){
+
+/*JOptionPane.showInputDialog(null,"hello,world");
+String ageText=JOptionPane.showInputDialog(null,"how old are you");
+int age=Integer.parseInt(ageText);
+JOptionPane.showMessageDialog(null,age);*/
+
+
+JFrame frame =new JFrame();
+frame.setVisible(true);
+
+}
+
+
+}
